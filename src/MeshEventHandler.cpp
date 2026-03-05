dd12f0c66cfdd25952e4c7c09c05cde18ae225d7
Brian Fletcher <brian.jf.fletcher@gmail.com>
2026-03-05 22:44:24 +0000

    fixes ci failures for firmware repo

--- src/MeshEventHandler.cpp
+++ src/MeshEventHandler.cpp
@@ -1,7 +1,10 @@
 #include "mapps/MeshEventHandler.h"
 
 #include <atomic>
+
+#ifdef HAS_FREE_RTOS
 #include <mutex>
+#endif
 
 static std::atomic<MeshEventHandler *> globalHandler{nullptr};
 
@@ -15,17 +18,23 @@ MeshEventHandler *getGlobalMeshEventHandler()
     return globalHandler.load(std::memory_order_acquire);
 }
 
+#ifdef HAS_FREE_RTOS
 static std::mutex callbackMutex;
+#endif
 static AppReadyCallback appReadyCallback;
 
 void setAppReadyCallback(AppReadyCallback cb)
 {
+#ifdef HAS_FREE_RTOS
     std::lock_guard<std::mutex> lock(callbackMutex);
+#endif
     appReadyCallback = std::move(cb);
 }
 
 AppReadyCallback getAppReadyCallback()
 {
+#ifdef HAS_FREE_RTOS
     std::lock_guard<std::mutex> lock(callbackMutex);
+#endif
     return appReadyCallback;
 }
